11eaccb0118ef25d535d8a6bdf082fb0c1feb5f0
Ram <Sriram003@e.ntu.edu.sg>
2018-07-02 16:25:53 +1000

    Audit Service Integration

---
+++ router-service/src/main/java/com/payment/router/model/AuditMessage.java
@@ -0,0 +1,61 @@
+package com.payment.router.model;
+
+public class AuditMessage {
+
+	private String id;
+	private String service;
+	private String statusCode;
+	private String statusDesc;
+	private String timestamp;
+
+	public AuditMessage(String id, String service, String statusCode,
+			String statusDesc, String timestamp) {
+		super();
+		this.id = id;
+		this.service = service;
+		this.statusCode = statusCode;
+		this.statusDesc = statusDesc;
+		this.timestamp = timestamp;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getService() {
+		return service;
+	}
+
+	public void setService(String service) {
+		this.service = service;
+	}
+
+	public String getStatusCode() {
+		return statusCode;
+	}
+
+	public void setStatusCode(String statusCode) {
+		this.statusCode = statusCode;
+	}
+
+	public String getStatusDesc() {
+		return statusDesc;
+	}
+
+	public void setStatusDesc(String statusDesc) {
+		this.statusDesc = statusDesc;
+	}
+
+	public String getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(String timestamp) {
+		this.timestamp = timestamp;
+	}
+
+}
