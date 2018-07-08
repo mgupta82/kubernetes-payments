2bb9f8cc47d0c9284d452b6c433e6ea2f549ac09
mgupta82 <mukeshsgupta@gmail.com>
2018-07-08 08:48:40 -0400

    fix persistence service

--- build.sh
+++ build.sh
@@ -6,19 +6,25 @@ cd ..
 
 cd persistance-api
 
-mvn dockerfile:build
+mvn clean install dockerfile:build -DskipTests
+
+cd ..
+
+cd ValidationService
+
+mvn clean install dockerfile:build -DskipTests
 
 cd ..
 
 cd router-service
 
-mvn clean install dockerfile:build
+mvn clean install dockerfile:build -DskipTests
 
 cd ..
 
 cd kafka-consumer-audit-service
 
-mvn clean install dockerfile:build
+mvn clean install dockerfile:build -DskipTests
 
 cd ..
 
