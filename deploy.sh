4460ca1fd741e1196b0ef7d67a5b966b15819d60
mgupta82 <mukeshsgupta@gmail.com>
2018-07-12 23:23:30 -0400

    Add persistence api fabric8 plugin for openshift

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,12 @@
+eval $(minishift oc-env)
+
+eval $(minishift docker-env)
+
 oc login
 
 oc new-project payment --display-name="payment"
 
 oc project payment
-
-#oc new-app --name=persistencedb https://github.com/mgupta82/payment.git --context-dir=mysql strategy=docker
 
 oc new-app -e MYSQL_USER=test MYSQL_PASSWORD=test MYSQL_DATABASE=projectdb registry.access.redhat.com/rhscl/mysql-56-rhel7 --name=persistencedb
 
@@ -12,13 +14,13 @@ cd mysql
 
 source run.sh
 
+sleep 5
+
 oc rollout latest persistencedb
 
-oc new-app --name=persistanceservice openshift/redhat-openjdk18-openshift:1.2~https://github.com/mgupta82/payment.git --context-dir=persistance-api  strategy=source
+cd ..
 
-oc expose svc/persistanceservice
+cd persistance-api
 
-oc new-app --name=transformationservice openshift/redhat-openjdk18-openshift:1.2~https://github.com/mgupta82/payment.git --context-dir=TransformationService  strategy=source
-
-oc expose svc/transformationservice
+mvn clean install fabric8:deploy -Popenshift -DskipTests
 
--- undeploy.sh
+++ undeploy.sh
@@ -2,11 +2,9 @@ oc login
 
 oc project payment
 
-oc delete all --selector app=persistanceservice
-
 oc delete all --selector app=persistencedb
 
-oc delete all --selector app=transformationservice
+oc delete all --selector app=persistance-api
 
 oc delete project payment
 
