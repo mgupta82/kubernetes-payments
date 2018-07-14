1870c321b6c94274e6eda28d6438ad4350a590a6
mgupta82 <mukeshsgupta@gmail.com>
2018-07-14 00:28:39 -0400

    add fabric8 plugin

--- deploy.sh
+++ deploy.sh
@@ -24,3 +24,32 @@ cd persistance-api
 
 mvn clean install fabric8:deploy -Popenshift -DskipTests
 
+cd ..
+
+oc new-app --name="routerdb" \
+    -e MONGODB_USER=test \
+    -e MONGODB_PASSWORD=test \
+    -e MONGODB_DATABASE=router \
+    -e MONGODB_ADMIN_PASSWORD=secret \
+    registry.access.redhat.com/rhscl/mongodb-32-rhel7
+
+oc create sa amq-service-account
+
+oc policy add-role-to-user view system:serviceaccount:payment:amq-service-account
+
+oc new-app --name=activemq \
+-e AMQ_USER=admin \
+-e AMQ_PASSWORD=admin \
+-e AMQ_TRANSPORTS=openwire,amqp,stomp,mqtt \
+-e AMQ_QUEUES=pacs.002.001.09.response.queue,pacs.008.001.07.request.queue \
+-e AMQ_MESH_DISCOVERY_TYPE=dns \
+-e AMQ_MESH_SERVICE_NAME=activemq \
+-e AMQ_MESH_SERVICE_NAMESPACE=payment \
+-e AMQ_STORAGE_USAGE_LIMIT="1 gb" \
+registry.access.redhat.com/jboss-amq-6/amq63-openshift
+
+cd router-service
+
+mvn clean install fabric8:deploy -Popenshift -DskipTests
+
+cd ..
