315f1763374eccce88e9c8ea3bdfc60b9892c79f
Ram <Sriram003@e.ntu.edu.sg>
2018-07-17 12:50:30 +1000

    added audit message to producer

---
+++ kafka-producer-audit-service/src/main/java/com/payment/router/model/AuditMessage.java
@@ -0,0 +1,65 @@
+package com.payment.router.model;
+
+public class AuditMessage {
+
+	private String messageId;
+	private String service;
+	private String statusCode;
+	private String statusDesc;
+	private String timestamp;
+
+
+	public AuditMessage(String messageId, String service, String statusCode, String statusDesc, String timestamp) {
+		super();
+		this.messageId = messageId;
+		this.service = service;
+		this.statusCode = statusCode;
+		this.statusDesc = statusDesc;
+		this.timestamp = timestamp;
+	}
+
+	public AuditMessage() {
+		super();
+	}
+
+	public String getService() {
+		return service;
+	}
+
+	public void setService(String service) {
+		this.service = service;
+	}
+
+	public String getStatusCode() {
+		return statusCode;
+	}
+
+	public void setStatusCode(String statusCode) {
+		this.statusCode = statusCode;
+	}
+
+	public String getStatusDesc() {
+		return statusDesc;
+	}
+
+	public void setStatusDesc(String statusDesc) {
+		this.statusDesc = statusDesc;
+	}
+
+	public String getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(String timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public String getMessageId() {
+		return messageId;
+	}
+
+	public void setMessageId(String messageId) {
+		this.messageId = messageId;
+	}
+
+}
